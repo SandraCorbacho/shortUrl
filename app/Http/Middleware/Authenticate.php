dab1f3eeb6a14e647f00dc8927ae885984474cab
Sandra <sandra.corbacho@thatzad.com>
2023-06-17 18:29:46 +0200

    bearer regex isValid

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -10,11 +10,24 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
+
     protected function redirectTo(Request $request): ?string
     {
-        if($request->bearerToken()){
+        $bearerToken =  $request->bearerToken();
+        if($this->isValidBearer($bearerToken)){
            return $request->json('url');
         }
         return $request->expectsJson() ? null : route('login');
+    }
+
+    function isValidBearer($bearerToken): bool
+    {
+
+        $regex = '/\(\)|\{\}|\[\]/';
+
+        while (preg_match($regex, $bearerToken)) {
+            $bearerToken = preg_replace($regex, '', $bearerToken);
+        }
+        return empty($bearerToken);
     }
 }
